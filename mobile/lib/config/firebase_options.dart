1b471c79f7ef21602aae4d7c61c7e22826171f97
tanmay34567 <167678109+tanmay34567@users.noreply.github.com>
2026-07-23 18:40:12 +0530

    feat: add Firebase configuration options for project initialization

---
+++ mobile/lib/config/firebase_options.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class DefaultFirebaseOptions {
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: "AIzaSyBjRYud7zhvZw2cxuwPOzJZjtpiIRx7Q0Y",
+    authDomain: "app-qr-f5e62.firebaseapp.com",
+    projectId: "app-qr-f5e62",
+    storageBucket: "app-qr-f5e62.firebasestorage.app",
+    messagingSenderId: "335566921346",
+    appId: "1:335566921346:web:58bd79c446b60b35e19e9b",
+    measurementId: "G-WR1M19MSNV",
+  );
+}
